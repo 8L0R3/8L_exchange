b706ca70e9b1096eb5f5ebfd27c11664c8c1ae1c
8L0R3 <51509291+8L0R3@users.noreply.github.com>
2024-09-05 01:54:55 +0000

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,45 +1,36 @@
 Config = {}
 
--- Discord webhook for notifications
-Config.DiscordWebhook = "YOUR_WEBHOOK" -- Replace with your webhook URL
+Config.DiscordWebhook = "Change WebHook Here"
 
--- Ped Config
 Config.Ped = {
-    model = "s_m_m_movspace_01",                       -- Ped Model
-    name = "Interact with ped",                        -- Ped Name (QB-Target)
-    position = vector3(45.62, -904.44, 29.97 - 0.99),  -- Ped Position
-    heading = 341.21                                   -- Ped heading
+    model = "s_m_m_movspace_01", 
+    name = "Interact",        
+    position = vector3(45.62, -904.44, 29.97 -0.99), 
+    heading = 341.21      
 }
 
--- Config trade Item for Car
 Config.VehicleTrades = {
-    ["BMW M8"] = {                        -- Trade name
-        vehicle = "mansm8",               -- Car model
-        label = "BMW M8",                 -- Item name in menu
-        itemsRequired = { coin = 3000 }   -- Item name = amount
+    ["BMW M8"] = {
+        vehicle = "mansm8",
+        label = "BMW M8",
+        itemsRequired = { coin = 3000 }
     },
     ["Continental"] = {
         vehicle = "17bcs",
         label = "Continental",
         itemsRequired = { coin = 4000 }
     },
-    ["Chiron"] = {
-        vehicle = "chironss",
-        label = "Chiron",
-        itemsRequired = { coin = 5000 }
-    },
-    -- Add here another vehicle
+    -- Add more
 }
 
--- Config trade Item for Item
+
 Config.ItemTrades = {
-    ["trade1"] = {                   -- Trade name
-        rewardItem = "case_recoil",  -- The item the player will receive
-        label = "Case Recoil",       -- Item name in menu
-        rewardAmount = 1,            -- The quantity of the item that the player will receive
+    ["trade1"] = {
+        rewardItem = "case_recoil",  
+        label = "Case Recoil",
+        rewardAmount = 1,            
         itemsRequired = {
-            ["coin"] = 500,          -- Item name = amount
-         -- ["bread"] = 2
+            ["coin"] = 500,          
         }
     },
     ["trade2"] = {
@@ -47,7 +38,7 @@ Config.ItemTrades = {
         label = "Case Prisma",
         rewardAmount = 1,            
         itemsRequired = {
-            ["coin"] = 800,           
+            ["coin"] = 800,          
         }
     },
     ["trade3"] = {
@@ -55,8 +46,8 @@ Config.ItemTrades = {
         label = "Case Falcion",
         rewardAmount = 1,            
         itemsRequired = {
-            ["coin"] = 1000,           
+            ["coin"] = 1000,          
         }
     },
-    -- Add here another item
+    -- Add more
 }
